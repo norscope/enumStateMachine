a8083543960e4c1c3483d82bf072e22095627df6
Alf Kristoffer Herland <alf@herland.as>
2020-09-12 19:53:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,52 @@
 
 class StateMachine{
 private:
-    enum States{
-        idle, receiveSingle, receiveStream, transmit,error
+    enum class States{
+        Idle, ReceiveSingle, ReceiveStream, Transmit,Error
     };
-    enum Transitions{
+    enum class Transitions{
         idleToTransmit,
         transmitToReceiveSingle, transmitToReceiveStream, transmitToError,
         receiveSingleToIdle, receiveSingleToTransmit, receiveSingleToError,
         receiveStreamToIdle, receiveStreamToTransmit, receiveStreamToError,
         errorToIdle
     };
-    enum Events{
+    enum class Events{
 
     };
+    States state = States::Idle;
 public:
    StateMachine(){}
-   ~StateMachine();
+   ~StateMachine(){};
+
+   void runStates(){
+        switch (state) {
+            case States::Idle:
+                std::cout << "enter idle \n";
+                break;
+            case States::ReceiveSingle:
+                std::cout << "enter ReceiveSingle \n";
+                break;
+            case States::ReceiveStream:
+                std::cout << "enter ReceiveStream \n";
+                break;
+            case States::Transmit:
+                std::cout << "enter Transmit \n";
+                break;
+            case States::Error:
+                std::cout << "enter Error \n";
+                break;
+        }
+   }
+
+   void runTransitions(){
+
+   }
 };
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    StateMachine m;
+    m.runStates();
     return 0;
 }
