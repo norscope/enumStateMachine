574dca2a2f2ec3372b9093b258b638ecc5e357f0
Alf.Herland <alf.herland@student.uib.no>
2020-09-13 19:46:50 +0200

    Added documentation and a few events to the machine

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # enumStateMachine
+This is a class that contains a state-machine that is based on enums.
+The state-machine implements a custom protocol over UDP.
+
+## Table that shows states,Events and transisions
+The cells in the table shows both legal and illegal transisions that can occur in the 
+state-machine. 
+
+|Events/States  |Idle    |Transmit      |Receive_single|Receive_stream|Error|
+|:-------------:|:------:|:------------:|:------------:|:------------:|:---:|
+|Set_RQS        |Transmit|x             |x             |x             |x    |
+|Set_RQFS       |Transmit|x             |x             |x             |x    |
+|Set_RQR        |Transmit|x             |x             |x             |x    |
+|Set_GS         |Transmit|x             |x             |x             |x    |
+|Set_ABRT       |x       |x             |x             |Transmit      |x    |
+|Set_THROTTLE   |Transmit|x             |x             |Transmit      |x    |
+|Set_Retry      |Transmit|x             |x             |x             |x    |
+|Set_Poll_Again |Transmit|x             |Transmit      |Transmit      |x    |
+|Get_WRITE      |x       |Receive_single|x             |x             |x    |
+|Get_STREAM     |x       |Receive_stream|x             |x             |x    |
+|Get_EOS        |x       |x             |x             |x             |x    |
+|Get_STATUS     |x       |Receive_single|x             |x             |x    |
+|SOCK_ERROR     |x       |Error         |Error         |x             |x    |
+|Got_Data       |x       |x             |Idle          |x             |x    |
+|DisplayError   |x       |x             |x             |x             |Idle |
--- main.cpp
+++ main.cpp
@@ -3,51 +3,100 @@
 class StateMachine{
 private:
     enum class States{
-        Idle, ReceiveSingle, ReceiveStream, Transmit,Error
+        /** The idle state is entry state of the FSM. Handles events to setting the
+         * OP code, if retry is set and if poll again is set.
+         */
+        Idle,
+        /**
+         * The receive single state  responsible for handling server replies that
+         * is only a single packet
+         */
+        ReceiveSingle,
+        /**
+         * The receive stream is responsible for server stream and full stream
+         * packets.
+         */
+        ReceiveStream,
+        /**
+         * The transmit state handles sending OP codes to the server
+         */
+        Transmit,
+
+        Error
     };
     enum class Transitions{
-        idleToTransmit,
-        transmitToReceiveSingle, transmitToReceiveStream, transmitToError,
-        receiveSingleToIdle, receiveSingleToTransmit, receiveSingleToError,
-        receiveStreamToIdle, receiveStreamToTransmit, receiveStreamToError,
-        errorToIdle
+      //Idle
+        IdleToTransmit,
+      //Transmit
+        TransmitToReceiveSingle, TransmitToReceiveStream, TransmitToError,
+      //ReceiveSingle
+        ReceiveSingleToIdle, ReceiveSingleToTransmit, ReceiveSingleToError,
+      //ReceiveStream
+        ReceiveStreamToIdle, ReceiveStreamToTransmit, ReceiveStreamToError,
+      //Error
+       ErrorToIdle
     };
     enum class Events{
-
+      Set_RQS,
+      Set_RQFS,
+      Set_RQR,
+      Set_GS,
+      Set_ABRT,
+      Set_THROTTLE,
+      Set_Retry,
+      Set_Poll_Again,
+      Get_WRITE,
+      Get_STREAM,
+      Get_EOS,
+      Get_STATUS,
+      SOCK_ERROR,
+      Got_Data,
+      DisplayError,
+      Idle
     };
     States state = States::Idle;
-public:
+    Events event = Events::Idle;
+  public:
    StateMachine(){}
    ~StateMachine(){};
 
-   void runStates(){
-        switch (state) {
-            case States::Idle:
-                std::cout << "enter idle \n";
-                break;
-            case States::ReceiveSingle:
-                std::cout << "enter ReceiveSingle \n";
-                break;
-            case States::ReceiveStream:
-                std::cout << "enter ReceiveStream \n";
-                break;
-            case States::Transmit:
-                std::cout << "enter Transmit \n";
-                break;
-            case States::Error:
-                std::cout << "enter Error \n";
-                break;
-        }
+   void Run_States(){
+     bool run = true;
+     while(run) {
+       switch (state) {
+       case States::Idle:
+         std::cout << "enter idle \n";
+         switch (event) {
+         case Events::Set_RQS:
+           state = States::Transmit;
+           break;
+         }
+         break;
+       case States::ReceiveSingle:
+         std::cout << "enter ReceiveSingle \n";
+         break;
+       case States::ReceiveStream:
+         std::cout << "enter ReceiveStream \n";
+         break;
+       case States::Transmit:
+         std::cout << "enter Transmit \n";
+         run = false;
+         break;
+       case States::Error:
+         std::cout << "enter Error \n";
+         break;
+       }
+     }
    }
-
-   void runTransitions(){
-
+   void Set_Start_Event(){
+    event = Events::Set_RQS;
    }
 };
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     StateMachine m;
-    m.runStates();
+    m.Set_Start_Event();
+    m.Run_States();
     return 0;
 }
